31edf885766dfbc34b59bb4cf66f8f93a032790d
ajayns <ajay.ns@outlook.com>
2018-02-06 01:20:04 +0530

    Add redux abstractions

---
+++ src/actions/index.js
@@ -0,0 +1,26 @@
+// Action creators recieve data from the DOM event, format it as formal JSON 'action' object
+
+import { ADD_TODO, SET_FILTER, TOGGLE_TODO } from '../constants/action-types';
+
+export const addTodo = (text) => {
+    return {
+        type: ADD_TODO,
+        id: nextTodoId++,
+        completed: false,
+        text
+    }
+}
+
+export const setFilter = (filter) => {
+    return {
+        type: SET_FILTER,
+        filter
+    }
+}
+
+export const toggleTodo = (id) => {
+    return {
+        type: TOGGLE_TODO,
+        id
+    }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,29 @@
+// Reducer functions take state from Redux and action objects and returns a new state
+// Each action is defined as a case
+
+import { ADD_TODO, SET_FILTER, TOGGLE_TODO } from '../constants/action-types';
+
+const todo = (state, action) => {
+    switch(action.type) {
+        case ADD_TODO:
+            return [...state,
+                    {  
+                        id: action.id,
+                        text: action.text,
+                        completed: action.completed
+                    }          
+                ];
+
+        case TOGGLE_TODO:
+            return state.map(todo => {
+                    if(todo.id === action.id) {
+                        return Object.assign({},todo, {completed: !todo.completed});
+                    }
+                    return todo;
+                }
+            );
+        
+        default:
+            return state;
+    }
+}
